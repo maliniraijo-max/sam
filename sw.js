3fef3e02ea5e4210cf984c81ba43e9c83cc65ee3
maliniraijo-max <maliniraijo@gmail.com>
2026-09-25 18:35:24 +0530

    Refresh search assets after web search redesign

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sam-learning-shell-v24";
+const CACHE_NAME = "sam-learning-shell-v25";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -16,7 +16,7 @@ const APP_SHELL = [
   "./explore.html",
   "./site-viewer.html",
   "./site-viewer.js?v=20260925-001",
-  "./explore.js?v=20260925-007"
+  "./explore.js?v=20260925-008"
 ];
 
 self.addEventListener("install", event => {
